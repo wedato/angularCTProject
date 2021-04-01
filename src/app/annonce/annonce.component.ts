6f32f28fcb62c9b9f2fe065634213d2bac3ab1ef
Baltaci Jonathan <jonathan.baltaci@etu.univ-orleans.fr>
2021-04-01 13:01:43 +0200

    "question 4 , update readme"

---
+++ src/app/annonce/annonce.component.ts
@@ -0,0 +1,35 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-annonce',
+  templateUrl: './annonce.component.html',
+  styleUrls: ['./annonce.component.css']
+})
+export class AnnonceComponent implements OnInit {
+
+   title = 'titre de base';
+   content = 'content de base';
+
+
+  // annonce: Array<{machin:string,}> = [
+  //   {machin:'thug'}
+  // ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  geTitle(): string {
+    return this.title;
+  }
+
+  getContent(): string {
+    return this.content;
+  }
+
+  editAnnonce(f) {
+
+  }
+}
